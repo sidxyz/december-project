1f58c062fd906aff7121e0ba90381ad08b0022a1
owalekarnikita <nikitao8984@gmail.com>
2017-01-27 15:21:02 +0530

    updated contactus page

--- collaboration/Assets/js/script.js
+++ collaboration/Assets/js/script.js
@@ -20,3 +20,19 @@ jQuery("document").ready(function($){
          pause: false
      		});
 });
+
+$(document).ready(function(){
+	$('window').mousein(.css('background-color','grey'));
+});
+
+/*$("#button").click(function(){
+    $("div").animate({left: '250px'});
+});
+/*$("button").click(function(){
+    $("div").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px'
+    });
+});*/
